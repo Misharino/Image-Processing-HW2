a8333dd5666676ac3ee4ba8a3d2e2e55f618202f
Misharino <michaelsvidsky@gmail.com>
2024-07-29 23:39:43 +0300

    Added image 2 and did the entire process for it again, added doc. Finishing touches.

--- edgeFilter.m
+++ edgeFilter.m
@@ -1,11 +1,11 @@
 function newImag = edgeFilter(imag)
-    % Edge detecting filter using sobel kernel.
+    % edge detecting filter using sobel kernel.
     image = double(imag);
         
     Gx = [-1 0 1; -2 0 2; -1 0 1];
     Gy = [1 2 1; 0 0 0; -1 -2 -1];
     
-    % Apply Sobel operators
+    % applying Sobel operators
     Ix = conv2(image, Gx, 'same');
     Iy = conv2(image, Gy, 'same');
     
